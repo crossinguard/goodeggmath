319b8fdf7053f403df3524a5cd78b44a8b789f38
crossinguard <w.brett.egbert@gmail.com>
2024-03-13 11:32:08 -0700

    updated good egg branding and colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,11 @@ export default defineConfig({
   site: 'https://goodeggmath.com/',
   integrations: [starlight({
     title: 'good egg',
+    logo: {
+      light: './src/assets/logo-sm.svg',
+      dark: './src/assets/logo-light-sm.svg',
+      replacesTitle: true,
+    },
     // social: {
     //   github: 'https://github.com/withastro/starlight'
     // },
