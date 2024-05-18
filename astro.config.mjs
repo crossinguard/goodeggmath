7fbf06bd8b92ca10e8460978e9def6570d70a9bd
Brett Egbert <brett@crossinguard.dev>
2024-05-17 18:55:56 -0700

    redo front page to prep for tutoring

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://goodeggmath.com/',
@@ -9,37 +11,56 @@ export default defineConfig({
     logo: {
       light: './src/assets/logo-sm.svg',
       dark: './src/assets/logo-light-sm.svg',
-      replacesTitle: true,
+      replacesTitle: true
     },
     social: {
       github: 'https://github.com/crossinguard'
     },
     customCss: [
-      // Relative path to your custom CSS file
-      './src/styles/custom.css',
-    ],
+    // Relative path to your custom CSS file
+    './src/styles/custom.css'],
     sidebar: [{
       label: 'Getting Started',
-      link: '/getting-started/',
+      link: '/getting-started/'
     }, {
       label: 'Types of Numbers',
-      items: [
-        {label: 'Overview', link: '/learn/number-types/overview'},
-      ],
-      badge: {text: 'under construction'}
+      items: [{
+        label: 'Overview',
+        link: '/learn/number-types/overview'
+      }],
+      badge: {
+        text: 'under construction'
+      }
     }, {
       label: 'Arithmetic Operations',
-      items: [
-        {label: 'Overview', link: '/learn/arithmetic-operations/overview'},
-        {label: 'Addition', link: '/learn/arithmetic-operations/addition'},
-        {label: 'Subtraction', link: '/learn/arithmetic-operations/subtraction'},
-        {label: 'Multiplication', link: '/learn/arithmetic-operations/multiplication'},
-        {label: 'Division', link: '/learn/arithmetic-operations/division'},
-        {label: 'Floor Division', link: '/learn/arithmetic-operations/floor-division'},
-        {label: 'Modulus', link: '/learn/arithmetic-operations/modulus'},
-        {label: 'Exponents', link: '/learn/arithmetic-operations/exponents'}
-      ],
-      badge: {text: 'under construction'}
+      items: [{
+        label: 'Overview',
+        link: '/learn/arithmetic-operations/overview'
+      }, {
+        label: 'Addition',
+        link: '/learn/arithmetic-operations/addition'
+      }, {
+        label: 'Subtraction',
+        link: '/learn/arithmetic-operations/subtraction'
+      }, {
+        label: 'Multiplication',
+        link: '/learn/arithmetic-operations/multiplication'
+      }, {
+        label: 'Division',
+        link: '/learn/arithmetic-operations/division'
+      }, {
+        label: 'Floor Division',
+        link: '/learn/arithmetic-operations/floor-division'
+      }, {
+        label: 'Modulus',
+        link: '/learn/arithmetic-operations/modulus'
+      }, {
+        label: 'Exponents',
+        link: '/learn/arithmetic-operations/exponents'
+      }],
+      badge: {
+        text: 'under construction'
+      }
     }, {
       label: 'Reference',
       autogenerate: {
@@ -47,7 +68,7 @@ export default defineConfig({
       }
     }, {
       label: 'About',
-      link: '/about/',
+      link: '/about/'
     }]
-  })]
+  }), icon()]
 });
